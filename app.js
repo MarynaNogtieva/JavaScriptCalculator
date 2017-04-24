24e0aa7fb9c8c0c067efbe85a4eb88b09b5a5c34
MarynaNogtieva <marynanogtieva@yahoo.ca>
2017-04-23 20:48:10 -0400

    Added functionality to display values on the screen

--- app.js
+++ app.js
@@ -2,16 +2,48 @@ var operation,
 	zero;
 
 var result = document.getElementById("result");
-var history = document.getElementById("historyResult");
-var numbers = document.querySelectorAll(".numberValue ");
-console.log(numbers);
+var historyRes = document.getElementById("historyResult");
 
+
+
+var numbers = document.querySelectorAll(".numberValue ");
+var allInputs = document.querySelectorAll(".calc");
+
+console.log(allInputs);
 
 var numbersAmount = numbers.length;
+var allInputsAmount = allInputs.length;
 
+function clearAll(){
+	result.innerHTML = "0";
+	historyRes.innerHTML = "0";
+
+}
+
+// get number value to the result, without displaying operations
 for(var i = 0; i <numbersAmount;i++){
 	numbers[i].addEventListener("click",function(e){
-        console.log(e.target.value);
+        if(result.innerHTML === "0"){
+        	result.innerHTML = "";
+        }
         result.innerHTML +=e.target.value;
+	},false);
+}
+
+// get all calues to display input history (including operations)
+for(var j = 0; j < allInputsAmount; j++){
+	allInputs[j].addEventListener("click",function(e){
+		
+		 if(historyRes.innerHTML === "0"){
+        	historyRes.innerHTML = "";
+       	 }
+
+        historyRes.innerHTML += e.target.value;
+       
+         if(e.target.value === "clear"){
+        		clearAll();
+        }
+	
+       
 	},false);
 }
